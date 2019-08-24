8aa03dc09f8a93ce672c285e0bde5416faadc135
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2019-08-24 12:08:12 +0300

    Update kNN_bang.py

--- kNN_bang.py
+++ kNN_bang.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
 # ##### Для отладки алгоритма и сравнения результатов использовалась учебная выборка с kaggle
 
 # Задача на kaggle: https://www.kaggle.com/c/bioresponse
 # Данные: https://www.kaggle.com/c/bioresponse/data
-# По данным характеристикам молекулы требуется определить, будет ли дан биологический ответ (biological response). 
-# Признаки нормализованы.
+# По данным характеристикам молекулы требуется определить, будет ли дан биологический ответ (biological response). Признаки нормализованы.
 
 # ##### Вызов алгоритма
 # from kNN_bang import KNN
@@ -18,14 +18,14 @@
 #test_base = KNN()			# данный метод создает объект класса с базовыми настройками 
 #test_base.fit(X_train, y_train)	# данный метод загружатет обучающую выборку и целевой вектор
 #test_base.predict(X_test)		# данный метод создает вектор предсказаний целевых значений
-#test_base.accuracy() 			# данный метод считает точность предсказаний
+#test_base.accuracy(y_test) 			# данный метод считает точность предсказаний
 
 # запуск алгоритма при 3 соседях, без применения весов, и расчете дистанции
 # по Эвклидовой метрике
 #test_uniform = KNN(n_neighbors=3, weights='uniform', p=2)
 #test_uniform.fit(X_train, y_train)
 #test_uniform.predict(X_test)
-#test_uniform.accuracy()
+#test_uniform.accuracy(y_test)
 
 
 # запуск алгоритма при 5 соседях, применении весов, и расчете дистанции
@@ -33,14 +33,16 @@
 #test_distance = KNN(n_neighbors=5, weights='distance', p=2)
 #test_distance.fit(X_train, y_train)
 #test_distance.predict(X_test)
-#test_distance.accuracy()
+#test_distance.accuracy(y_test)
 
 # запуск алгоритма при 5 соседях, применении весов, и расчете дистанции
 # по Манхэттенской метрике
 #test_distance_man = KNN(n_neighbors=5, weights='distance', p=1)
 #test_distance_man.fit(X_train_50, y_train_50)
 #test_distance_man.predict(X_test_25)
-#test_distance_man.accuracy()
+#test_distance_man.accuracy(y_test)
+
+"""
 
 import pandas as pd
 import numpy as np
@@ -48,22 +50,16 @@ from collections import defaultdict, Counter
 import operator
 
 class KNN:
-    # это метод инициализации объекта данного класса, куда мы передаем
-    # количество ближайших, метод расчета весов при них(с учетом удаленности) или без весов,
-    # и способ расчета расстояния (эвклидов или манхеттенский)
-    # по дефолту считаем по 5 бижайшим соседям, веса не применяем, 
-    # метод расчета расстояний - Эвклидов
+ 
     def __init__(self, n_neighbors=5, weights='uniform', p=2):
         self.n_neighbors = n_neighbors
         self.weights = weights
         self.p = p
-    # поскольку это т.н. ленивый классификатор, то в процессе обучения он не делает ничего, 
-    # а только хранит тренировочные данные. Он начинает классификацию только тогда, когда 
-    # появляются новые немаркированные данные.  
+
     def fit(self, X_train, y_train):
         self.X_train = np.array(X_train)
         self.y_train = np.array(y_train)
-    # метод предсказывает принадлежность к классу      
+ 
     def predict(self, X_test):
         p = self.p
         n_neighbors = self.n_neighbors
@@ -74,63 +70,45 @@ class KNN:
         pediction_vector = []
         for i in range(len(X_test)): 
             dictance_between_objects_array = []
-            
-            # расчет Эвклидовой метрики
             if p == 2:
                 for j in range(len(X_train)):
                     e_dist = np.linalg.norm(X_test[i] - X_train[j])
-                    # формируем массив из дистанций до классифицируемого объекта
                     dictance_between_objects_array.append(e_dist)
-                
-            # расчет Манхэттенской метрики        
             elif p == 1:
                 for j in range(len(np.array(X_train))):
                     m_dist = map(lambda x,y: math.fabs(float(x)-float(y)),
                                  np.array(X_test)[i],np.array(X_train)[j]) 
                     m_dist = sum(m_dist)/100.
-                    # формируем массив из дистанций до классифицируемого объекта
                     dictance_between_objects_array.append(m_dist)
-                           
-            # задаем словарь из пар "расстояние"-"класс"
             neighbors_dictionary = {}
             for i, j in zip(dictance_between_objects_array, y_train):
                 neighbors_dictionary.setdefault(i,[]).append(j)
-            # сортируем ключи-расстояния в порядке возрастания и формируем из них массив
             sorted_distances = sorted(neighbors_dictionary.keys())
-            # технический массив
             self.sorted_distances = sorted_distances
             
             neighbors_list = []
             if weights == 'uniform':
-            # формируем массив из n_neighbors классов с наименьшими дистанциями до классифицируемого 
-            # объекта
                 for g in range(n_neighbors):
                     neighbors_list.append(neighbors_dictionary.get(sorted_distances[g]))
                     
                 neighbor = []
                 for h in neighbors_list:    
-                    neighbor.append(h[0])            
-                # считаем количество объектов разных классов
+                    neighbor.append(h[0]) 
                 counter = Counter(neighbor)
                 prediction = max(counter.iteritems(), key=operator.itemgetter(1))[0]
-                # формируем вектор прогнозов класса объектов тестовой выборки 
                 pediction_vector.append(prediction)
                 self.pediction_vector = pediction_vector    
             
-                  
-            # формируем массив из n_neighbors классов с учетом удаленности
             elif weights == 'distance':
                 short_neighbors_dictionary = {}
                 short_key = []
                 short_values = []
-                # веса считаем как частное: 1/(порядковый номер по отношению к целевому объекту)
                 for g in range(n_neighbors):
                     short_key.append(1/float(g+1))
                     short_values.append(neighbors_dictionary.get(sorted_distances[g])[0])
-                # получаем словарь где ключи это классы а значения - массив весов
                 for i, j in zip(short_key, short_values):
                     short_neighbors_dictionary.setdefault(j,[]).append(i) 
-                # 
+                
                 sum_dict = {}
                 sum_key = []
                 sum_value = []
@@ -142,10 +120,10 @@ class KNN:
                 sorted_weights = sorted(sum_dict.items())
                 pediction_vector.append(sorted_weights[-1][1])
                 self.pediction_vector = pediction_vector        
+    def accuracy(self, y_test):
+	pediction_vector = self.pediction_vector 
+	self.y_test = np.array(y_test)
         
-    # метод считает точность предсказания 
-    def accuracy(self):
-        pediction_vector = self.pediction_vector
         right_prediction = 0
         for i in range(len(pediction_vector)):
             if pediction_vector[i] == y_test[i]:
@@ -153,5 +131,5 @@ class KNN:
             else:
                 pass
         print 'KNN accuracy ', float(right_prediction)/len(pediction_vector)
-        
+
 
